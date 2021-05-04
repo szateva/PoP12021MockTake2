e0c1eea80c9023314f063d700f7786b9684fa72d
Eva Szatmari <szateva@gmail.com>
2021-05-04 11:41:29 +0100

    question5.py is not working :( question6.py is started

---
+++ question5.py
@@ -0,0 +1,43 @@
+# 13021326
+
+""" Implement a non-fruitful function
+trim_matrix(M)
+that, given a list M of lists M[0], M[1], . . . , modifies M so that it forms a correct matrix.
+The modification should make all of the M[i] of the same length by removing from the
+end of each M[i] a number of elements n, such that M[i] is longer than the shortest
+list among M[0], M[1], . . . by n. Importing any libraries is NOT allowed.
+Indicative test cases:
+X = [[1,2,3],[12,13],[21,22,23,24]]
+trim_matrix(X)
+assert X == [[1,2],[12,13],[21,22]] #must be True
+X = [[1,2,3],[12,13,14]]
+trim_matrix(X)
+assert X == [[1,2,3],[12,13,14]] #must be True
+X = [[1,2,3],[12,13,14],[]]
+trim_matrix(X)
+assert X == [[],[], []] #must be True """
+
+def trim_matrix(M):
+    # finding the shortest row:
+    shortest = len(M[0])
+    print(shortest)
+    for row in range(len(M)):
+        if len(M[row]) < shortest:
+            shortest = len(M[row])
+            print(shortest)
+
+    for row in range(len(M)):
+        if len(M[row]) > shortest:
+            del M[shortest:]
+    return M
+
+# Indicative test cases:
+X = [[1,2,3],[12,13],[21,22,23,24]]
+trim_matrix(X)
+assert X == [[1,2],[12,13],[21,22]] #must be True
+X = [[1,2,3],[12,13,14]]
+trim_matrix(X)
+assert X == [[1,2,3],[12,13,14]] #must be True
+X = [[1,2,3],[12,13,14],[]]
+trim_matrix(X)
+assert X == [[],[], []] #must be True
---
+++ question6.py
@@ -0,0 +1,23 @@
+# 13021326
+
+""" Implement a function
+day_of_year(d, m)
+that takes as parameters the number d in the range 1..31 representing a day, and
+the number m in the range 1..12 representing a month of the year 2020. It returns
+how many complete days passed from the beginning of the fist day of 2020 until the
+specified date d/m/2020. Note that 2020 is a leap year, so February has 29 days. The
+function must return -1 if the specified date d/m/2020 is not a valid date. Importing
+any libraries is NOT allowed.
+Indicative test cases:
+assert day_of_year(3,1)==2 #must be True
+assert day_of_year(10,5)==130 #must be True
+assert day_of_year(31,6)==-1 #must be True"""
+
+def day_of_year(d, m):
+    pass
+
+
+# Indicative test cases:
+assert day_of_year(3,1)==2 #must be True
+assert day_of_year(10,5)==130 #must be True
+assert day_of_year(31,6)==-1 #must be True
